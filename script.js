75b18949fbe2cfe5cc1e2ad639f85b8abfe35775
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-15 15:33:16 +0100

    add initial js file for my calculator business logic

---
+++ script.js
@@ -0,0 +1,20 @@
+//###################################################################
+
+
+
+
+//Business logic for my calculator 
+
+function add(number1, number2) {
+return  number1+number2
+}
+
+// alert(add(10,12))
+
+let number1 = parseInt(prompt(" type any number" ));
+let number2 = parseInt(prompt(" type another number"));
+        
+
+// invoking on the function 
+
+document.write(add(number1,number2));
